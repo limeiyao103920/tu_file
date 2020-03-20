6787251649e83d300cf42933d64ab1bd44b01fc4
limeiyao <71733035@qq.com>
2020-03-20 15:35:31 +0800

    图片上传

--- src/main/java/com/maven/web/controller/Zuoye.java
+++ src/main/java/com/maven/web/controller/Zuoye.java
@@ -1,16 +1,42 @@
 package com.maven.web.controller;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class Zuoye {
 
-	@RequestMapping("list")
+	@RequestMapping("admin")
+	
 	public String list() {
 		
 		return "list";
 	}
 	
+	@RequestMapping("add")
+	
+	public String add(Model model,MultipartFile file) {
+		if(!file.isEmpty()) {
+		String path="e:/pic/";
+		String filename = file.getOriginalFilename();
+		File file2=new File(path,filename);
+		model.addAttribute("filename",filename);
+			try {
+				file.transferTo(file2);
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		
+		
+		}
+		
+		return "success";
+	}
 	
 	
 }
--- src/main/webapp/WEB-INF/view/list.jsp
+++ src/main/webapp/WEB-INF/view/list.jsp
@@ -1,18 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-<!-- 引入 css -->
-<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resource/bootstrap.css">
-<!-- 引入js -->
-<script type="text/javascript" src="${pageContext.request.contextPath}/resource/jquery-3.2.1.js"></script>
 </head>
 <body>
-作业1
+	<form action="${pageContext.request.contextPath}/add" method="post" enctype="multipart/form-data">
+		<input type="file" name="file">
+		<input type="submit" value="提交">
+	</form>
 </body>
 
 </html>
